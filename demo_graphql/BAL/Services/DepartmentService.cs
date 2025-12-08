7c507e17a991c10b993564c899fee0a7ea7a8899
alpesh-vaghasiya <94340114+alpesh-vaghasiya@users.noreply.github.com>
2025-12-08 12:32:39 +0530

    Revert "dashboard summary api added"
    This reverts commit 29e17e08758da4ba68620c3b79409b298bcc7607.

--- demo_graphql/BAL/Services/DepartmentService.cs
+++ demo_graphql/BAL/Services/DepartmentService.cs
@@ -5,21 +5,21 @@ using static demo_graphql.Controllers.QueryInspector;
 
 namespace demo_graphql.Services
 {
-    public class EmployeeService : Controllers.IEmployeeService
+    public class DepartmentService : Controllers.IDepartmentService
     {
         private readonly string _connectionString;
 
-        public EmployeeService(IConfiguration configuration)
+        public DepartmentService(IConfiguration configuration)
         {
             _connectionString = configuration.GetConnectionString("DefaultConnection")
                 ?? throw new InvalidOperationException("Connection string missing.");
         }
 
-        public async Task<Response> GetDashboardSummary()
+        public async Task<Response> GetAllAsync()
         {
             Response _response = new();
             using var connection = new NpgsqlConnection(_connectionString);
-            var result = await connection.QueryAsync<DashboardSummary>(PostGresQuery.Get_dashboard_summary);
+            var result = await connection.QueryAsync<Department>(PostGresQuery.Get_department);
 
             _response.data = result;
             _response.responseMessages.Add(new ResponseMessage { type = "S", message = "Success" });
--- demo_graphql/Extension/ServiceExtensions.cs
+++ demo_graphql/Extension/ServiceExtensions.cs
@@ -20,7 +20,7 @@ public static class ServiceExtensions
         services.AddScoped<IHasuraService, HasuraService>();
         services.AddScoped<IWorkFlowService, WorkFlowService>();
         services.AddScoped<IValidationService, ValidationService>();
-        services.AddScoped<IEmployeeService, EmployeeService>();
+        services.AddScoped<IDepartmentService, DepartmentService>();
     }
 
     public static void ConfigureSwagger(this IServiceCollection services)
